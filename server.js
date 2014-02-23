772c3d6ce763f8dea2f2ac49a44983f6c74edb08
Richard Harrington <rwharrington87@gmail.com>
2014-02-22 22:59:36 -0500

    add basic chat server and client

--- server.js
+++ server.js
@@ -4,3 +4,23 @@ var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 
 server.listen(3000);
+
+app.use(express.static('./web'));
+
+var sockets = [];
+
+io.sockets.on('connection', function (socket) {
+
+  sockets.push(socket);
+
+  socket.on('message:send', function (data) {
+    sockets.forEach(function (socket) {
+      socket.emit('message:receive', data);
+    });
+    console.log('RECEIVED A MESSAGE: ' + data.message);
+  });
+
+  socket.on('disconnect', function () {
+    sockets.splice(sockets.indexOf(socket), 1);
+  });
+});
